63c9a5cc65db177b9c307e9527cbe086d1529056
Carter <johnston.j.carter@gmail.com>
2023-07-26 20:39:49 -0400

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,45 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<script lang="ts">
+
+    interface HypeLink {
+        protocol: string;
+        domain: string;
+        pathnames: string[];
+        searchParams: IterableIterator<[string, string]> | [];
+    }
+    let urlToDisplay : HypeLink;
+
+    function generateUrl(this : HTMLInputElement) {
+        if(!this.value) {
+            return;
+        }
+        try {
+            const url = new URL(this.value);
+
+            urlToDisplay = {
+                protocol: url.protocol,
+                domain: url.host,
+                pathnames: url.pathname.split('/'),
+                searchParams: url.searchParams.entries() ?? []
+            }
+            urlToDisplay = urlToDisplay;
+        }
+        catch(err) {
+
+        }
+    }
+</script>
+
+<input type="text" on:change={generateUrl}>
+
+<p>{urlToDisplay?.domain ?? "-"}</p>
+
+
+
+
+
   
 <style lang="postcss">
-:global(html) {
-    background-color: theme(colors.gray.100);
-}
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
 </style>
